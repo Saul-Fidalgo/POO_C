07bd1d031574fe04ab8ae418ad172eb2da4da461
Jeff Fidalgo <jzacariasf@miumg.edu.gt>
2022-02-13 11:36:00 -0600

    POO_C

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+#include "Cliente.cpp"
+#include <iostream>
+using namespace std;
+main(){
+	string nit,nombres,apellidos,direccion;
+	int telefono;
+	cout<<"Ingrese Nit: ";
+	cin>>nit;
+	cout<<"Ingrese Nombres: ";
+	cin>>nombres;
+	cout<<"Ingrese Apellidos: ";
+	cin>>apellidos;
+	cout<<"Ingrese Direccion: ";
+	cin>>direccion;
+	cout<<"Ingrese Telefono: ";
+	cin>>telefono;
+
+	Cliente obj = Cliente(nombres,apellidos,direccion,telefono,nit);
+	obj.mostrar();
+	
+	cout<<"Ingrese Nit: ";
+	cin>>nit;
+	obj.setNit(nit);
+	obj.mostrar();
+
+}
